288596827a73b954800117dda3a45eb36ed7fcd1
bartexpl11 <175180@student.uwm.edu.pl>
2024-03-10 13:32:55 +0100

    zmiana

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-print("hello")
-print("hello")
-print("hello")
-print("hello")
-print("hello")asd
+try:
+    a = int(input())
+    b = int(input())
+    result = a/b
+    print(result)
+except ZeroDivisionError:
+    print("Error")
+except ValueError:
+    print("Wrong value")
+except Exception:
+    print("Error na wszytskie bledy")
+else:
+    print(result)
